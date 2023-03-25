0b1ff52557d0b95d03118272649c87cbbe8604b0
Lazaro21 <olsf21@gmail.com>
2023-03-25 14:42:22 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/eddie/workshopmongo/domain/User.java
+++ src/main/java/com/eddie/workshopmongo/domain/User.java
@@ -1,8 +1,14 @@
 package com.eddie.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 
+@Document("user")
 public class User implements Serializable {
+
+    @Id
     private String id;
     private String name;
     private String email;
---
+++ src/main/java/com/eddie/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.eddie.workshopmongo.services;
+
+import com.eddie.workshopmongo.domain.User;
+import com.eddie.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
